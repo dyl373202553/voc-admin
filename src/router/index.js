3071e6d81fbfbce8f06b4479cb4ea2a53bd4f484
YaLiDai <dyacd@cn.ibm.com>
2020-09-16 09:56:10 +0800

    跳过登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -32,19 +32,9 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
-
-  {
     path: '/404',
     component: () => import('@/views/404'),
     hidden: true
-  },
-  {
-    path: '/',
-    redirect: '/dashboard'
   },
   {
     path: '/',
