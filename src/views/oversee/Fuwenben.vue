8f0ba1769d5a293d753a379547c637c5a9281a5c
YaLiDai <dyacd@cn.ibm.com>
2020-09-22 22:01:10 +0800

    添加富文本

---
+++ src/views/oversee/Fuwenben.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <editor-bar v-model="detail" :is-clear="isClear" @change="change" />
+  </div>
+</template>
+<script>
+import EditorBar from "../../components/wangEnditor/Editoritem"
+export default {
+    components: { EditorBar },
+    data() {
+        return {
+            isClear: false,
+            detail: ""
+        }
+    },
+    methods: {
+        change(val) {
+            console.log(val)
+        }
+    }
+}
+</script>
