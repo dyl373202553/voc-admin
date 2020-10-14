8ac90b87cfe40992985e0c1da4213b2c0deba9d5
YaLiDai <dyacd@cn.ibm.com>
2020-10-14 16:39:34 +0800

    首页的排行榜

--- src/views/IndexPage/Rank.vue
+++ src/views/IndexPage/Rank.vue
@@ -4,45 +4,46 @@
         <span class="header-title">排行榜</span>
       </div>
       <div class="home-tabs">
-        <el-tabs v-model="rankList" type="card" @tab-click="handleClick">
+        <!-- <el-tabs v-model="rankList" type="card" @tab-click="handleClick"> -->
+        <el-tabs v-model="rankList" type="card">
           <el-tab-pane label="点赞数" name="first">
             <div class="home-table-tabs">
               <el-tabs v-model="likenum">
                 <el-tab-pane label="按二级部门数据排行" name="first">
-                  <el-table :data="tableData">
-                    <el-table-column prop="rank" label="排名" align="center" width="200">
+                  <el-table :data="rankListData">
+                    <el-table-column prop="rankingNum" label="排名" align="center" width="200">
                       <div slot-scope="scope">
-                        <img v-show="scope.row.rank === '1'" src="@/assets/images/no1.png" class="image">
-                        <img v-show="scope.row.rank === '2'" src="@/assets/images/no2.png" class="image">
-                        <img v-show="scope.row.rank === '3'" src="@/assets/images/no3.png" class="image">
-                        <span v-show="scope.row.rank === '4'" class="drank">4</span>
-                        <span v-show="scope.row.rank === '5'" class="drank">5</span>
+                        <img v-show="scope.row.rankingNum === 1" src="@/assets/images/no1.png" class="image">
+                        <img v-show="scope.row.rankingNum === 2" src="@/assets/images/no2.png" class="image">
+                        <img v-show="scope.row.rankingNum === 3" src="@/assets/images/no3.png" class="image">
+                        <span v-show="scope.row.rankingNum === 4" class="drank">4</span>
+                        <span v-show="scope.row.rankingNum === 5" class="drank">5</span>
                       </div>
                     </el-table-column>
-                    <el-table-column prop="department" label="部门" align="center" />
-                    <el-table-column prop="likenum" label="点赞数" align="center">
+                    <el-table-column prop="praiseDept2Name" label="部门" align="center" />
+                    <el-table-column prop="praiseDept2Num" label="点赞数" align="center">
                       <div slot-scope="scope">
-                        共<span class="dred total">{{ scope.row.likenum }}</span>次
+                        共<span class="dred total">{{ scope.row.praiseDept2Num }}</span>次
                       </div>
                     </el-table-column>
                   </el-table>
                 </el-tab-pane>
                 <el-tab-pane label="按个人数据排行" name="second">
-                  <el-table :data="tableData">
-                    <el-table-column prop="rank" label="排名" align="center" width="200">
+                  <el-table :data="rankListData">
+                    <el-table-column prop="rankingNum" label="排名" align="center" width="200">
                       <div slot-scope="scope">
-                        <img v-show="scope.row.rank === '1'" src="@/assets/images/no1.png" class="image">
-                        <img v-show="scope.row.rank === '2'" src="@/assets/images/no2.png" class="image">
-                        <img v-show="scope.row.rank === '3'" src="@/assets/images/no3.png" class="image">
-                        <span v-show="scope.row.rank === '4'" class="drank">4</span>
-                        <span v-show="scope.row.rank === '5'" class="drank">5</span>
+                        <img v-show="scope.row.rankingNum === 1" src="@/assets/images/no1.png" class="image">
+                        <img v-show="scope.row.rankingNum === 2" src="@/assets/images/no2.png" class="image">
+                        <img v-show="scope.row.rankingNum === 3" src="@/assets/images/no3.png" class="image">
+                        <span v-show="scope.row.rankingNum === 4" class="drank">4</span>
+                        <span v-show="scope.row.rankingNum === 5" class="drank">5</span>
                       </div>
                     </el-table-column>
-                    <el-table-column prop="name" label="姓名" align="center" />
-                    <el-table-column prop="department" label="部门" align="center" />
-                    <el-table-column prop="likenum" label="点赞数" align="center">
+                    <el-table-column prop="praiseUserName" label="姓名" align="center" />
+                    <el-table-column prop="praiseUserDeptName" label="部门" align="center" />
+                    <el-table-column prop="praiseUserNum" label="点赞数" align="center">
                       <div slot-scope="scope">
-                        共<span class="dred total">{{ scope.row.likenum }}</span>次
+                        共<span class="dred total">{{ scope.row.praiseUserNum }}</span>次
                       </div>
                     </el-table-column>
                   </el-table>
@@ -58,40 +59,40 @@
             <div class="home-table-tabs">
               <el-tabs v-model="activeNametabs">
                 <el-tab-pane label="按二级部门数据排行" name="first">
-                  <el-table :data="tableData">
-                    <el-table-column prop="rank" label="排名" align="center" width="200">
+                  <el-table :data="rankListData">
+                    <el-table-column prop="rankingNum" label="排名" align="center" width="200">
                       <div slot-scope="scope">
-                        <img v-show="scope.row.rank === '1'" src="@/assets/images/no1.png" class="image">
-                        <img v-show="scope.row.rank === '2'" src="@/assets/images/no2.png" class="image">
-                        <img v-show="scope.row.rank === '3'" src="@/assets/images/no3.png" class="image">
-                        <span v-show="scope.row.rank === '4'" class="drank">4</span>
-                        <span v-show="scope.row.rank === '5'" class="drank">5</span>
+                        <img v-show="scope.row.rankingNum === 1" src="@/assets/images/no1.png" class="image">
+                        <img v-show="scope.row.rankingNum === 2" src="@/assets/images/no2.png" class="image">
+                        <img v-show="scope.row.rankingNum === 3" src="@/assets/images/no3.png" class="image">
+                        <span v-show="scope.row.rankingNum === 4" class="drank">4</span>
+                        <span v-show="scope.row.rankingNum === 5" class="drank">5</span>
                       </div>
                     </el-table-column>
-                    <el-table-column prop="department" label="部门" align="center" />
-                    <el-table-column prop="likenum" label="点赞数" align="center">
+                    <el-table-column prop="commentDept2Name" label="部门" align="center" />
+                    <el-table-column prop="commentDept2Num" label="点赞数" align="center">
                       <div slot-scope="scope">
-                        共<span class="dred total">{{ scope.row.likenum }}</span>次
+                        共<span class="dred total">{{ scope.row.commentDept2Num }}</span>次
                       </div>
                     </el-table-column>
                   </el-table>
                 </el-tab-pane>
                 <el-tab-pane label="按个人数据排行" name="second">
-                  <el-table :data="tableData">
-                    <el-table-column prop="rank" label="排名" align="center" width="200">
+                  <el-table :data="rankListData">
+                    <el-table-column prop="rankingNum" label="排名" align="center" width="200">
                       <div slot-scope="scope">
-                        <img v-show="scope.row.rank === '1'" src="@/assets/images/no1.png" class="image">
-                        <img v-show="scope.row.rank === '2'" src="@/assets/images/no2.png" class="image">
-                        <img v-show="scope.row.rank === '3'" src="@/assets/images/no3.png" class="image">
-                        <span v-show="scope.row.rank === '4'" class="drank">4</span>
-                        <span v-show="scope.row.rank === '5'" class="drank">5</span>
+                        <img v-show="scope.row.rankingNum === 1" src="@/assets/images/no1.png" class="image">
+                        <img v-show="scope.row.rankingNum === 2" src="@/assets/images/no2.png" class="image">
+                        <img v-show="scope.row.rankingNum === 3" src="@/assets/images/no3.png" class="image">
+                        <span v-show="scope.row.rankingNum === 4" class="drank">4</span>
+                        <span v-show="scope.row.rankingNum === 5" class="drank">5</span>
                       </div>
                     </el-table-column>
-                    <el-table-column prop="name" label="姓名" align="center" />
-                    <el-table-column prop="department" label="部门" align="center" />
-                    <el-table-column prop="likenum" label="点赞数" align="center">
+                    <el-table-column prop="commentUserName" label="姓名" align="center" />
+                    <el-table-column prop="commentUserDeptName" label="部门" align="center" />
+                    <el-table-column prop="commentUserNum" label="点赞数" align="center">
                       <div slot-scope="scope">
-                        共<span class="dred total">{{ scope.row.likenum }}</span>次
+                        共<span class="dred total">{{ scope.row.commentUserNum }}</span>次
                       </div>
                     </el-table-column>
                   </el-table>
@@ -146,18 +147,16 @@ export default class Home extends Vue {
         pageSize: "10"
     }
 
+    private rankListData = []
+
     protected mounted() {
         this.load()
-    }
-
-    private handleClick(tab: any, event: any) {
-        console.log(tab, event)
     }
 
     private load() {
         getRankList().then((res) => {
             if (res) {
-                console.log(res)
+                this.rankListData = res.data
             }
         })
     }
