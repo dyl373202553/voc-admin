a68be076076b7af62908c18196a444a28aa17d0f
YaLiDai <dyacd@cn.ibm.com>
2021-01-05 21:25:45 +0800

    发布特别关注

--- src/views/specialFocus/SpecialFocus.vue
+++ src/views/specialFocus/SpecialFocus.vue
@@ -1,5 +1,5 @@
 <template v-if="this.userrole==='0'">
-    <div class="app-container">
+    <div class="app-container release">
         <el-card class="box-card">
             <div slot="header" class="clearfix">
                 <span class="header-title">特别关注</span>
@@ -52,69 +52,18 @@
                     :rules="[
                     { required: true, message: '内容上传不能为空'}
                     ]"
-                    :class="(showFile === 1)? 'imgList': ''">
-                    <el-upload
-                        v-show="$route.params.viewStatus !== '3'"
-                        class="upload-image"
-                        :action="' '"
-                        list-type="picture"
-                        accept=".jpg,.png,.jpeg,.mp4,.wma"
-                        :auto-upload="false"
-                        :on-exceed="handleExceed"
-                        :on-remove="handleRemove"
-                        :limit="1"
-                        :show-file-list="false"
-                        :file-list="fileDataList"
-                        :on-change="handleAvatarChangeIcon"
-                        ref="uploadicon"
-                        >
-                        <el-button size="small" class="dprogress-btn" slot="tip" type="danger" plain @click="upbtn" v-show="showFile === 1">上传</el-button>
-                        <el-button size="small" type="primary" plain v-show="showFile === 0">选择文件</el-button>
-                        <span class="dgrey" slot="tip" style="margin-left:20px;">请上传小于150M的文件，支持格式png/jpg/mp4/wma</span>
-                    </el-upload>
-                    <!-- 编辑 -->
-                    <template v-if="dataListFile.length!==0">
-                        <div :class="progressPercent===100? 'dataListFile opacity1':'dataListFile opacity4'">
-                            <i class="el-icon-close del" @click="updel"></i>
-                            <template>
-                              <el-image :src="dataListFile[0].content" v-if="this.dataForm.type === 'img'" fit="contain" class="image"/>
-                              <video :src="dataListFile[0].content" v-if="this.dataForm.type === 'video'" :controls="true">
-                                  您的浏览器不支持视频播放
-                              </video>
-                            </template>
-                        </div>
-                    </template>
-
-                    <el-progress v-show="progressFlag" :stroke-width="9" type="line" :color="customColors" :percentage="progressPercent" :status="progressStatus"></el-progress>
-                    <!-- 详情 -->
-                    <el-col :span="24" v-if="$route.params.id && $route.params.viewStatus === '3'">
-                        <div class="dimg-div">
-                            <el-image :src="`/resources/`+ dataForm.content" fit="contain" class="image" v-if="this.dataForm.type === 'img'" />
-                            <video :src="`/resources/`+ dataForm.content" :controls="true" v-if="this.dataForm.type === 'video'">
-                                您的浏览器不支持视频播放
-                            </video>
-                        </div>
-                    </el-col>
-                </el-form-item>
-                <el-form-item label="封面上传"  prop="logoUrl"
-                    v-show="this.dataForm.type === 'video'"
-                    :rules="[
-                    { required: true, message: '内容上传不能为空'}
-                    ]"
                   >
-                    <el-upload
-                        class="upload-cover"
-                        :action="' '"
-                        list-type="picture"
-                        ref="upload"
-                        accept=".jpg,.png,.jpeg"
-                        :auto-upload="true"
-                        :http-request="uploadRequest"
-                        :limit="1"
-                        >
-                        <el-button size="small" type="primary" plain>选择文件</el-button>
-                        <span class="dgrey" slot="tip" style="margin-left:20px;">请上传小于150M的文件，支持格式png/jpg</span>
-                    </el-upload>
+                  <editorImage color="#1890ff" class="editor-upload-btn" @successCBK="imageSuccessCBK" />
+                  <editorVideo color="#1890ff" class="editor-upload-btn" @successCBK="imageSuccessCBK" />
+                  <span class="dgrey" style="margin-left:20px;">请上传小于150M的文件，支持格式png/jpg/mp4/wma</span>
+                </el-form-item>
+                <el-form-item>
+                  <div style="width:75%;height:247px; overflow: hidden;">
+                      <el-image v-show="coverArr.type==='img'" :src="coverArr.cover" fit="contain" class="image" />
+                      <video v-show="coverArr.type==='video'" :src="coverArr.url" :poster="coverArr.cover" :controls="true">
+                          您的浏览器不支持视频播放
+                      </video>
+                  </div>
                 </el-form-item>
                 <el-form-item v-show="$route.params.viewStatus !== '3'" class="dbtn text-center">
                     <el-button type="primary" round @click="onSubmit" v-show="!this.over"
@@ -134,16 +83,20 @@ import { Component, Vue } from "vue-property-decorator"
 import { postSpecialFocus, getSpecialFocusDetail, postOffLine } from "@/api/specialFocus/SpecialFocus"
 import { MessageBox } from "element-ui"
 import { day } from "@/lib/js/unitls"
+import editorImage from './Upload/EditorImage.vue'
+import editorVideo from './Upload/EditorVideo.vue'
 import { UserModule } from "@/store/module/user"
 import axios from "axios"
 import Cookies from "js-cookie"
-@Component
+@Component({
+    components: { editorImage, editorVideo }
+})
 export default class SpecialFocus extends Vue {
     get userToken() {
         // @ts-ignore
         return UserModule.token
     }
-
+    private coverArr: any = {}
     private dialogFormVisible = false
     private over = false
     private dataForm = {
@@ -160,7 +113,6 @@ export default class SpecialFocus extends Vue {
     // 图片
     private dfile: any
     private showFile = 0
-    private fileDataList: any = []
     private progressPercent = 0
     private progressFlag = false
     private progressStatus: any = null
@@ -206,13 +158,10 @@ export default class SpecialFocus extends Vue {
         getSpecialFocusDetail(params).then((res) => {
             if (res) {
                 this.dataForm = res.data
-                this.fileDataList = [{ name: this.dataForm.content, url: `/resources/` + this.dataForm.content }]
-                const arr = {
-                    content: ""
-                }
                 if (this.$route.params.viewStatus !== "3") {
-                    arr.content = `/resources/` + this.dataForm.content
-                    this.dataListFile.push(arr)
+                    this.coverArr.url = this.dataForm.content
+                    this.coverArr.cover = this.dataForm.cover
+                    this.coverArr.type = this.dataForm.type
                 }
             }
         })
@@ -258,164 +207,6 @@ export default class SpecialFocus extends Vue {
         })
     }
 
-    // 上传图片
-    private handleAvatarChangeIcon(file: any, fileList: any) {
-        const arr = {
-            content: ""
-        }
-        arr.content = file.url
-        this.dataListFile.push(arr)
-        const isJPG = file.raw.type === "image/jpeg"
-        const isPNG = file.raw.type === "image/png"
-        const isMP4 = file.raw.type === "video/mp4"
-        const isWMA = file.raw.type === "video/wma"
-        const isLt150M = file.raw.size / 1024 / 1024 <= 150
-        if (file.raw.type.indexOf("image") !== -1) {
-            this.dataForm.type = "img"
-        } else if (file.raw.type.indexOf("video") !== -1) {
-            this.dataForm.type = "video"
-        }
-
-        if (!isPNG && !isJPG && !isMP4 && !isWMA) {
-            this.fileDataList = []
-            this.$message.error("上传图片只能是 JPG/PNG 格式! 请重新选择")
-            return false
-        } else if (!isLt150M) {
-            this.fileDataList = []
-            this.$message.error("上传图片大小不能超过 150M! 请重新选择")
-            return false
-        } else if (isLt150M && (isPNG || isJPG || isMP4 || isWMA)) {
-            this.dfile = file
-            this.progressFlag = true
-            if (fileList.length > 0) {
-                this.showFile = 1
-            }
-        }
-    }
-
-    // 文件超出限制控制
-    private handleExceed() {
-        this.$message.warning("只能上传一个文件")
-    }
-
-    private handleRemove(file: any, fileList: any) {
-        const isJPG = file.raw.type === "image/jpeg"
-        const isPNG = file.raw.type === "image/png"
-        const isMP4 = file.raw.type === "video/mp4"
-        const isWMA = file.raw.type === "video/wma"
-        const isLt150M = file.raw.size / 1024 / 1024 <= 150
-        if (isLt150M && (isPNG || isJPG || isMP4 || isWMA)) {
-            if (fileList.length === 0) {
-                this.progressFlag = false
-                this.progressPercent = 0
-                this.showFile = 0
-            }
-        }
-    }
-
-    // 上传封面
-
-    private uploadRequest(option: any) {
-        const isLt = option.file.size / 1024 / 1024 <= 150
-        if (!isLt) {
-            option.onError()
-            this.$message.error("上传附件大小不能超过 150M! 请重新选择")
-            return false
-        } else if (isLt) {
-            const formData = new FormData()
-            formData.append("file", option.file) // 传参改为formData格式
-            axios({
-                method: "post",
-                url: `/vue-potal/portal-file/api/file/provider/resourcesUploadfile?busSource=moa-customervoice&filePath=khzsSpecialAttention&isystemName=2`, // 请求后端的url
-                headers: {
-                    "Content-Type": "multipart/form-data", // 设置headers
-                    Authorization: `Bearer ${this.userToken}`
-                },
-                data: formData,
-                onUploadProgress: (progressEvent) => {
-                    const num = progressEvent.loaded / progressEvent.total * 100 | 0 // 百分比
-                    option.onProgress({ percent: num }) // 进度条
-                }
-            })
-                .then((res: any) => {
-                    if (res) {
-                        if (res.data.code < 200) {
-                            // 上传成功
-                            this.dataForm.cover = res.data.data.filePath
-                            option.onSuccess() // 上传成功(打钩的小图标)
-                        }
-                    } else {
-                        // 上传失败
-                        option.onError("上传失败")
-                    }
-                })
-                .catch(() => {
-                    // 请求失败
-                    option.onError("上传失败")
-                })
-        }
-    }
-
-    private upbtn() {
-        const formData = new FormData()
-        formData.append("file", this.dfile.raw) // 传参改为formData格式
-        axios({
-            method: "post",
-            url: `/vue-potal/portal-file/api/file/provider/resourcesUploadfile?busSource=moa-customervoice&filePath=khzsSpecialAttention&isystemName=2`, // 请求后端的url
-            headers: {
-                "Content-Type": "multipart/form-data", // 设置headers
-                Authorization: `Bearer ${this.userToken}`
-            },
-            data: formData,
-            onUploadProgress: progressEvent => {
-                // progressEvent.loaded:已上传文件大小
-                // progressEvent.total:被上传文件的总大小
-                this.progressPercent = (progressEvent.loaded / progressEvent.total * 100)
-            }
-        })
-            .then((res: any) => {
-                if (res) {
-                    if (res.data.code < 200) {
-                        // 上传成功
-                        console.log(res.data.data)
-                        // 测试
-                        if (this.dataForm.type === "img") {
-                            console.log("img")
-                            this.dataForm.cover = res.data.data.filePath
-                        } else { // 视频
-                            console.log("video")
-                            console.log(this.dataForm.content)
-                            this.dataForm.content = res.data.data.filePath
-                        }
-                        
-                        if (this.progressPercent === 100) {
-                            // this.progressFlag = false
-                            this.showFile = 3
-                            this.progressStatus = "success"
-                        }
-                    }
-                } else {
-                    // 上传失败
-                    this.progressStatus = "exception"
-                }
-            })
-            .catch(() => {
-                // 请求失败
-                this.progressStatus = "warning"
-            })
-    }
-
-    private updel() {
-        this.dataListFile = []
-        this.fileDataList = []
-        this.dataForm.content = ""
-        this.dataForm.cover = ""
-        this.progressFlag = false
-        this.progressPercent = 0
-        this.showFile = 0
-        this.progressStatus = null
-    }
-
     // 校验时间
     private checkDate() {
         if (this.dataForm.startTime && this.dataForm.endTime) {
@@ -424,6 +215,17 @@ export default class SpecialFocus extends Vue {
                 this.dataForm.endTime = ""
             }
         }
+    }
+
+    private imageSuccessCBK(arr: any) {
+        console.log(arr)
+        Vue.set(this.coverArr, "cover", arr[0].cover)
+        Vue.set(this.coverArr, "type", arr[0].type)
+        Vue.set(this.coverArr, "url", arr[0].url)
+        console.log(this.coverArr)
+        this.dataForm.content = this.coverArr.url
+        this.dataForm.type  = this.coverArr.type
+        this.dataForm.cover  = this.coverArr.cover
     }
 }
 </script>
---
+++ src/views/specialFocus/Upload/EditorImage.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="upload-container">
+    <svg t="1609833136005" @click=" dialogVisible=true" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2361" width="20" height="20"><path d="M973.609745 750.236863 853.432379 750.236863 853.432379 630.039569c0-26.709513-20.033381-46.743891-46.743891-46.743891s-46.743891 20.034377-46.743891 46.743891l0 126.87542L626.391052 756.914989c-26.709513 0-46.743891 20.033381-46.743891 46.743891s20.033381 46.743891 46.743891 46.743891l126.87542 0 0 126.855492c0 26.709513 20.033381 46.743891 46.743891 46.743891 26.709513 0 46.743891-20.034377 46.743891-46.743891L846.754254 850.40277 973.609745 850.40277c26.711506 0 46.743891-20.032384 46.743891-46.743891C1027.031761 776.949366 1000.322248 750.236863 973.609745 750.236863L973.609745 750.236863 973.609745 750.236863zM505.222231 871.609954 505.222231 871.609954 87.137882 871.609954c-15.500585 0-27.135989-11.634407-27.135989-27.175846l0-759.112174c0-15.511546 11.635403-27.145953 27.135989-27.145953l740.858431 0c15.47966 0 27.11606 11.635403 27.11606 27.145953l0 410.313131 0 0 0 0c0 15.500585 11.656329 27.136985 27.176843 27.136985 15.499589 0 27.135989-11.6364 27.135989-27.136985l0 0 0 0L909.425205 58.176977c0-31.031064-27.135989-58.178014-58.177017-58.178014L60.000897-0.001036C28.979797-0.001036 1.82388 27.145913 1.82388 58.176977l0 813.432977c0 31.001171 27.155918 58.157088 58.177017 58.157088l445.221334 0 0 0c15.499589 0 27.135989-11.656329 27.135989-27.155918C532.356226 883.224433 520.720823 871.609954 505.222231 871.609954L505.222231 871.609954 505.222231 871.609954zM499.513639 736.881608c20.033381-40.065765 66.777271-66.777271 113.521162-66.777271l53.42102 0 0-60.098149c0-53.422016 33.389632-100.165907 80.132526-120.198291l0 0 0 0-66.776275-200.319857-233.718457 300.485763-66.777271-106.843036L132.261565 736.881608 499.513639 736.881608 499.513639 736.881608 499.513639 736.881608zM379.316344 309.51046c0-40.054804-33.388636-73.444436-73.453404-73.444436s-73.454401 33.389632-73.454401 73.444436c0 40.066761 33.388636 73.454401 73.454401 73.454401S379.316344 349.577221 379.316344 309.51046L379.316344 309.51046 379.316344 309.51046z" p-id="2362" fill="#707070"></path></svg>
+    <el-dialog :visible.sync="dialogVisible">
+      <el-upload
+        :limit="1"
+        :file-list="fileList"
+        :show-file-list="true"
+        :on-remove="handleRemove"
+        :on-success="handleSuccess"
+        :before-upload="beforeUpload"
+        :on-change="changeImg"
+        :headers = headerUpload
+        class="editor-slide-upload video-upload"
+        action="/vue-potal/portal-file/api/file/provider/resourcesUploadfile?busSource=moa-customervoice&filePath=khzsSpecialAttention&isystemName=2"
+        list-type="picture-card"
+      >
+        <div class="cover-border" v-show="coverShow">
+            <el-button size="small" type="primary">
+              上传图片
+            </el-button>
+        </div>
+      </el-upload>
+      <el-button @click="dialogVisible = false">
+        取消
+      </el-button>
+      <el-button type="primary" @click="handleSubmit">
+        确认
+      </el-button>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+// import { getToken } from 'api/qiniu'
+import { UserModule } from "@/store/module/user"
+export default {
+    name: 'EditorSlideUpload',
+    props: {
+        color: {
+            type: String,
+            default: '#1890ff'
+        }
+    },
+    data() {
+        return {
+            dialogVisible: false,
+            listObj: {},
+            coverShow: true,
+            fileList: [],
+            headerUpload: {
+                Authorization: `Bearer ${UserModule.token}`
+            }
+        }
+    },
+    // computed:{
+    //   userToken:{
+    //       get:function(){
+    //           return UserModule.token
+    //       }
+    //   }
+    // },
+    methods: {
+        checkAllSuccess() {
+            console.log(this.listObj)
+            return Object.keys(this.listObj).every(item => this.listObj[item].hasSuccess)
+        },
+        handleSubmit() {
+            const arr = Object.keys(this.listObj).map(v => this.listObj[v])
+            console.log(arr)
+            console.log(this.checkAllSuccess())
+            if (!this.checkAllSuccess()) {
+                this.$message('请等待所有的文件上传完毕，如果网络有问题请再次刷新!')
+                return
+            }
+            this.$emit('successCBK', arr)
+            this.listObj = {}
+            this.fileList = []
+            this.dialogVisible = false
+            this.coverShow = true
+        },
+        handleSuccess(response, file) {
+            const uid = file.uid
+            console.log(this.listObj)
+            const objKeyArr = Object.keys(this.listObj)
+            console.log(response)
+            console.log(file)
+            console.log(objKeyArr)
+            for (let i = 0, len = objKeyArr.length; i < len; i++) {
+                console.log(objKeyArr[i])
+                console.log(this.listObj[objKeyArr[i]].uid)
+                if (this.listObj[objKeyArr[i]].uid === uid) {
+                    this.listObj[objKeyArr[i]].cover = `/resources/` + response.data.filePath
+                    this.listObj[objKeyArr[i]].hasSuccess = true
+                    return
+                }
+                console.log(this.listObj)
+            }
+        },
+        handleRemove(file) {
+            this.coverShow = true
+            const uid = file.uid
+            const objKeyArr = Object.keys(this.listObj)
+            for (let i = 0, len = objKeyArr.length; i < len; i++) {
+                if (this.listObj[objKeyArr[i]].uid === uid) {
+                    delete this.listObj[objKeyArr[i]]
+                    return
+                }
+            }
+        },
+        beforeUpload(file) {
+            const _self = this
+            const _URL = window.URL || window.webkitURL
+            const fileName = file.uid
+            this.listObj[fileName] = {}
+            return new Promise((resolve, reject) => {
+                const img = new Image() 
+                console.log(img)
+                img.src = _URL.createObjectURL(file)
+                img.onload = function() {
+                    _self.listObj[fileName] = { hasSuccess: false, uid: file.uid, type:"img" }
+                }
+                resolve(true)
+            })
+          },
+          changeImg(file) {
+            this.coverShow = false
+          }
+      }
+}
+</script>
+
+<style lang="scss" scoped>
+.editor-slide-upload {
+  margin-bottom: 20px;
+  ::v-deep .el-upload--picture-card {
+    width: 100%;
+  }
+}
+</style>
